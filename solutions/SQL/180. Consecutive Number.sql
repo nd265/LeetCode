54472627b0c6138b723ce1e034514f3a282b3fe4
Navya Dahiya <navyad265@gmail.com>
2022-01-08 13:04:51 -0800

    Add Distinct keyword

--- solutions/SQL/180. Consecutive Number.sql
+++ solutions/SQL/180. Consecutive Number.sql
@@ -2,7 +2,7 @@
 # author: Navya Dahiya
 # created: 8 Jan, 2022
 
-SELECT L1.num as ConsecutiveNums
+SELECT DISTINCT L1.num as ConsecutiveNums
     FROM Logs L1, Logs L2, Logs L3
     WHERE L1.num = L2.num AND L2.num = L3.num
         AND L1.id = L2.id-1
