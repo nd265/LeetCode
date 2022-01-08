b9f7ea7b674955a0922286bf5448ccd5a20a3a60
Navya Dahiya <navyad265@gmail.com>
2022-01-08 12:11:10 -0800

    Add 627. Swap Salary solution

---
+++ solutions/SQL/627. Swap Salary.sql
@@ -0,0 +1,9 @@
+# 627. Swap Salary (https://leetcode.com/problems/swap-salary/)
+# author: Navya Dahiya
+# created: 8 Jan, 2022
+
+UPDATE Salary
+SET sex = CASE 
+        WHEN sex = 'm' THEN 'f'
+        WHEN sex = 'f' THEN 'm'
+        END
